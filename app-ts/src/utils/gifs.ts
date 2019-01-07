4c097d1d29779324688177d8148e3ae5f9dccb1b
toddriley <toddriley@192.168.120.59>
2019-01-07 15:22:37 -0500

    Converted.

--- app-ts/src/utils/gifs.ts
+++ app-ts/src/utils/gifs.ts
@@ -1,4 +1,9 @@
-export const gifIds = [
+interface GifData {
+  id: string;
+  name: string;
+}
+
+export const gifIds: GifData[] = [
   { id: "", name: "" },
   { id: "xT4uQulxzV39haRFjG", name: "Tacos" },
   { id: "3og0IPxMM0erATueVW", name: "Fetch" },
