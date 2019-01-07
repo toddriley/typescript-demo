71ece97f9bf6062b04c1f2c8916487f0276933b6
toddriley <toddriley@192.168.120.59>
2019-01-07 16:42:47 -0500

    Converted.

--- app-ts/src/components/Main.tsx
+++ app-ts/src/components/Main.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import * as React from "react";
+import { ApiData } from "../utils/api";
 
-const Main = props => {
+interface MainProps {
+  isOriginal: boolean;
+  isHd: boolean;
+  isDownsized: boolean;
+  selectedId: string;
+  idsToData: { [key: string]: ApiData[] };
+}
+
+const Main = (props: MainProps) => {
   const gifs = [];
   let totalSize = 0;
   if (props.selectedId && props.idsToData[props.selectedId]) {
     for (const gif of props.idsToData[props.selectedId]) {
       if (props.isOriginal) {
-        const gifSize = gif.images.original.size;
+        const { size } = gif.images.original;
+        const gifSize = size ? parseInt(size) : 0;
         gifs.push({
           ...gif.images.original,
           key: gifSize + "o"
@@ -14,15 +24,17 @@ const Main = props => {
         totalSize += gifSize;
       }
       if (props.isDownsized) {
-        const gifSize = gif.images.downsized.size;
+        const { size } = gif.images.downsized;
+        const gifSize = size ? parseInt(size) : 0;
         gifs.push({
           ...gif.images.downsized,
           key: gifSize + "d"
         });
         totalSize += gifSize;
       }
-      if (props.isHd) {
-        const gifSize = gif.images.hd.size;
+      if (props.isHd && gif.images.hd) {
+        const { size } = gif.images.hd;
+        const gifSize = size ? parseInt(size) : 0;
         gifs.push({ ...gif.images.hd, key: gifSize + "hd" });
         totalSize += gifSize;
       }
